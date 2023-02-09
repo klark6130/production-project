7145e95a6c2e971100811d59046fed134bd70547
Dmitry Bastrikov <35004320+klark6130@users.noreply.github.com>
2023-02-09 22:02:58 +0300

    add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,17 @@ import { BuildOptions } from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[]{
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  }
+
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
     use: [
@@ -47,8 +58,9 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[]{
     exclude: /node_modules/,
   }
   return [
-    svgLoader,
     fileLoader,
+    svgLoader,
+    babelLoader,
     typescriptLoader,
     cssLoader
   ]
