2f0939d4a589d78a3fd4f9020765f562d7fd9107
Dmitry Bastrikov <crysis-demon@mail.ru>
2023-02-18 16:43:50 +0300

    fix problem when build storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -15,7 +15,8 @@ export default ({ config }: { config: webpack.Configuration }) => {
   config.resolve?.extensions?.push('.ts', '.tsx');
 
   config.module.rules = config.module?.rules?.map((rule: RuleSetRule) => {
-    if ((rule.test as string).includes('svg')) {
+    // eslint-disable-next-line @typescript-eslint/prefer-includes
+    if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
     return rule;
