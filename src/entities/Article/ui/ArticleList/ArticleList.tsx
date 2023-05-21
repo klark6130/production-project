7b57d259e0d30c5062b6d82ee4bf06ff25c72486
Dmitry Bastrikov <35004320+klark6130@users.noreply.github.com>
2023-05-21 23:04:50 +0300

    add react virualized

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -6,6 +6,8 @@ import { Article, ArticleView } from '../../model/types/article';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import { ArticleListItemSkeleton } from '../ArticleListItem/ArticleListItemSkeleton';
 import { Text, TextSize } from 'shared/ui/Text/Text';
+import { List, ListRowProps, WindowScroller } from 'react-virtualized';
+import { PAGE_ID } from 'widgets/Page/Page';
 
 interface ArticleListProps {
   className?: string
@@ -26,15 +28,38 @@ const getSkeletons = (view: ArticleView) => {
 export const ArticleList = memo(({ className, articles, view = ArticleView.TILE, isLoading, target }: ArticleListProps) => {
   const { t } = useTranslation('article');
 
-  const renderArticle = (article: Article) => {
+  const isBig = view === ArticleView.LIST;
+
+  const itemsPerRow = isBig ? 1 : 3;
+
+  const rowCount = isBig ? articles.length : Math.ceil(articles.length / itemsPerRow);
+
+  const rowRender = ({ index, key, style }: ListRowProps) => {
+    const items = [];
+    const fromIndex = index * itemsPerRow;
+    const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
+
+    for (let i = fromIndex; i < toIndex; i++) {
+      items.push(
+        <ArticleListItem 
+          article={articles[index]} 
+          view={view}
+          className={cls.card}
+          target={target}
+          key={articles[index].id}
+        />
+      )
+      
+    }
+
     return (
-      <ArticleListItem 
-        article={article} 
-        view={view}
-        className={cls.card}
-        key={article.id}
-        target={target}
-      />
+      <div 
+        key={key}
+        style={style}
+        className={cls.row}
+      >
+        {items}
+      </div>
     )
   }
 
@@ -47,12 +72,36 @@ export const ArticleList = memo(({ className, articles, view = ArticleView.TILE,
   }
 
   return (
-    <div className={classNames(cls.ArticleList, {}, [className, cls[view]]) }>
-      {articles.length > 0
-        ? articles.map(renderArticle)
-        : null}
-      {isLoading && getSkeletons(view) }
-    </div>
+    <WindowScroller
+      onScroll={() => console.log('scroll')}
+      scrollElement={document.getElementById(PAGE_ID) as Element}
+    >
+      {({ height, width, registerChild, onChildScroll, isScrolling, scrollTop }) => (
+        <div 
+          ref={registerChild}
+          className={classNames(cls.ArticleList, {}, [className, cls[view]]) }
+        >
+          <List
+            height={height ?? 700}
+            width={width ? width - 80 : 700}
+            rowCount={rowCount}
+            rowHeight={isBig ? 700 : 330}
+            rowRenderer={rowRender}
+            autoHeight
+            onScroll={onChildScroll}
+            isScrolling = {isScrolling}
+            scrollTop={scrollTop}
+          />
+          {isLoading && getSkeletons(view) }
+        </div>
+      )}
+    </WindowScroller>
+    // <div className={classNames(cls.ArticleList, {}, [className, cls[view]]) }>
+    //   {articles.length > 0
+    //     ? articles.map(renderArticle)
+    //     : null}
+    //   {isLoading && getSkeletons(view) }
+    // </div>
     
   )
 });
