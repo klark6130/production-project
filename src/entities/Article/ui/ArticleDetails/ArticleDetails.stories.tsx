adb033eb77e409dbd8c5eb950d6b7e57dff79046
Dmitry Bastrikov <35004320+klark6130@users.noreply.github.com>
2023-08-15 00:02:11 +0300

    update ui tests

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
@@ -20,7 +20,7 @@ const article = {
   id: '1',
   title: 'Javascript news',
   subtitle: 'Что нового в JS за 2022 год?',
-  img: 'https://teknotower.com/wp-content/uploads/2020/11/js.png',
+  img: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
   views: 1022,
   createdAt: '26.02.2022',
   type: [ArticleType.IT],
