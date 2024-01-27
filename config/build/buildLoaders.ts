d70563805c8dec18d2c051c4e5b071dfd973348a
Dmitry Bastrikov <35004320+klark6130@users.noreply.github.com>
2024-01-28 00:04:02 +0300

    fix icon size and color

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -20,7 +20,22 @@ export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/i,
     issuer: /\.[jt]sx?$/,
-    use: ['@svgr/webpack']
+    use: [{
+      loader: '@svgr/webpack',
+      options: {
+        icon: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'convertColors',
+              params: {
+                currentColor: true
+              }
+            }
+          ]
+        }
+      }
+    }]
   }
 
   const cssLoader = buildCssLoaders(isDev);
--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,11 +1,11 @@
 import { saveJsonSettings } from '@/entities/User';
-import DarkIcon from '@/shared/assets/icons/theme-dark.svg';
-import LightIcon from '@/shared/assets/icons/theme-light.svg';
+import ThemeIcon from '@/shared/assets/icons/theme-light.svg';
 import { Theme } from '@/shared/const/theme';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
 import { useTheme } from '@/shared/lib/hooks/useTheme';
 import { Button, ButtonTheme } from '@/shared/ui/Button';
+import { Icon } from '@/shared/ui/Icon';
 import { memo, useCallback } from 'react';
 
 interface ThemeSwitcherProps {
@@ -29,7 +29,7 @@ export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
             className={classNames('', {}, [className])}
             onClick={onToggleHandler}
         >
-            {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+            <Icon Svg={ThemeIcon} width={40} height={40} inverted />
         </Button>
     );
 });
