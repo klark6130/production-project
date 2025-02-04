46a109316ac12756457d2ec1af87f2d8e1535c78
Dmitry Bastrikov <35004320+klark6130@users.noreply.github.com>
2025-02-05 00:09:14 +0300

    add docker filter for json-server and front

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        svgr({
-            exportAsDefault: true,
-        }),
-        react(),
-    ],
-    resolve: {
-        alias: [
-            {
-                find: '@',
-                replacement: '/src',
-            },
+export default defineConfig(({mode}) => {
+
+    const env = loadEnv(mode, process.cwd(), '')
+
+    console.log('api url', env.apiUrl)
+    const apiUrl = JSON.stringify(env.apiUrl);
+
+    return {
+        plugins: [
+            svgr({
+                exportAsDefault: true,
+            }),
+            react(),
         ],
-    },
-    define: {
-        __IS_DEV__: JSON.stringify(true),
-        __API__: JSON.stringify('http://localhost:8000'),
-        __PROJECT__: JSON.stringify('frontend'),
-    },
+        resolve: {
+            alias: [
+                {
+                    find: '@',
+                    replacement: '/src',
+                },
+            ],
+        },
+        define: {
+            __IS_DEV__: JSON.stringify(true),
+            __API__: apiUrl || JSON.stringify('http://localhost:8000'),
+            __PROJECT__: JSON.stringify('frontend'),
+        },
+    }
 });
